b8c4d6e779f205447fccd7bda845674fc6c536d5
aegypius <git@aegypius.com>
2025-09-16 14:13:13 +0200

    test: use LazyObjectInterface on symfony 7.2

--- src/Doctrine/DecoratedEntityManager.php
+++ src/Doctrine/DecoratedEntityManager.php
@@ -8,15 +8,28 @@ use Doctrine\ORM\Decorator\EntityManagerDecorator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
-use Symfony\Component\DependencyInjection\Attribute\Lazy;
+use Symfony\Component\VarExporter\LazyObjectInterface;
+use Symfony\Component\VarExporter\LazyProxyTrait;
 
-#[Lazy, AsDecorator('doctrine.orm.default_entity_manager')]
-class DecoratedEntityManager extends EntityManagerDecorator
+#[AsDecorator('doctrine.orm.default_entity_manager')]
+class DecoratedEntityManager extends EntityManagerDecorator implements LazyObjectInterface
 {
+    use LazyProxyTrait;
+
     public function __construct(
         #[AutowireDecorated]
         EntityManagerInterface $wrapped
     ) {
         parent::__construct($wrapped);
+    }
+
+    #[\Override]
+    public function resetLazyObject(): bool
+    {
+        if ($this->wrapped instanceof LazyObjectInterface) {
+            return $this->wrapped->resetLazyObject();
+        }
+
+        return false;
     }
 }
