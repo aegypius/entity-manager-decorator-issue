202bc7c8561bfeaa8818d058c3439564100df09d
aegypius <git@aegypius.com>
2025-09-16 14:56:19 +0200

    test: fails with service marked as lazy with symfony 7.3

--- src/Doctrine/DecoratedEntityManager.php
+++ src/Doctrine/DecoratedEntityManager.php
@@ -8,28 +8,15 @@ use Doctrine\ORM\Decorator\EntityManagerDecorator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
-use Symfony\Component\VarExporter\LazyObjectInterface;
-use Symfony\Component\VarExporter\LazyProxyTrait;
+use Symfony\Component\DependencyInjection\Attribute\Lazy;
 
-#[AsDecorator('doctrine.orm.default_entity_manager')]
-class DecoratedEntityManager extends EntityManagerDecorator implements LazyObjectInterface
+#[Lazy, AsDecorator('doctrine.orm.default_entity_manager')]
+class DecoratedEntityManager extends EntityManagerDecorator
 {
-    use LazyProxyTrait;
-
     public function __construct(
         #[AutowireDecorated]
         EntityManagerInterface $wrapped
     ) {
         parent::__construct($wrapped);
-    }
-
-    #[\Override]
-    public function resetLazyObject(): bool
-    {
-        if ($this->wrapped instanceof LazyObjectInterface) {
-            return $this->wrapped->resetLazyObject();
-        }
-
-        return false;
     }
 }
